9836e552e1cb1e0a06cfec01d5931ed82060c7e7
Hiago Martins Jago <devj4g0@gmail.com>
2024-01-17 00:48:45 -0300

    Better script 1.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,44 +1,47 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key");
-const volumeSlider = document.querySelector(".knob");
-const keysCheck = document.querySelector(".keys-check");
+const states = {
+    view: {
+        showKeys: document.querySelector(".keys-check"),
+        volume: document.querySelector(".knob"),
+        pianoKeys: document.querySelectorAll(".piano-keys .key"),
+    },
+    value: {
+        volume: 0.5,
+        supportKeys: [],
+    },
+};
 
-let audio = new Audio("src/Audio/a.wav");
-
-let mapedKeys = [];
-
-const playTune = (key) => {
-    audio.src = `src/audio/${key}.wav`;
+const playSound = (key) => {
+    const audio = new Audio(`src/audio/${key}.wav`);
+    audio.volume = states.value.volume;
     audio.play();
 
-    const clickedKey = document.querySelector(`[data-key="${key}"]`)
-    clickedKey.classList.add("active")
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
     setTimeout(() => {
         clickedKey.classList.remove("active");
     }, 150);
 };
 
-
-pianoKeys.forEach((key) => {
-    key.addEventListener("click", () => playTune(key.dataset.key))
-    mapedKeys.push(key.dataset.key);
-});
-
-document.addEventListener("keydown",
-    (e) => {
-        if (mapedKeys.includes(e.key)) {
-            playTune(e.key);
-        }
-    });
-
-handleVolume = (e) => {
-    audio.volume = e.target.value;
-    console.log(e.target.value);
-}
-
-volumeSlider.addEventListener("input", handleVolume)
+const handleVolumeChange = (e) => {
+    states.value.volume = e.currentTarget.value;
+};
 
 const showHideKeys = () => {
-    pianoKeys.forEach(key => key.classList.toggle("hide"))
-}
+    states.view.pianoKeys.forEach((key) => {
+        key.classList.toggle("hide");
+    });
+};
 
-keysCheck.addEventListener("click", showHideKeys)
+states.view.pianoKeys.forEach((key) => {
+    key.addEventListener("click", () => playSound(key.dataset.key));
+    states.value.supportKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+    if (states.value.supportKeys.includes(e.key)) {
+        playSound(e.key);
+    }
+});
+
+states.view.volume.addEventListener("input", handleVolumeChange);
+states.view.showKeys.addEventListener("click", showHideKeys);
